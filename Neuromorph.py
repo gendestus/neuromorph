3c3a6f74824d1e095eb3ee77381c20440656b9b5
zach <zach@spinreactor.ai>
2024-01-15 12:26:57 -0500

    adds ability to create a custom name for the agent

--- Neuromorph.py
+++ Neuromorph.py
@@ -15,7 +15,10 @@ class Neuromorph:
     # Upon creation, the main class will load the desired model into memory and initialize the component parts of the brain
     # torch type will default to half precision because that's what works with mistral on consumer grade gpus for now
     # display_thoughts defaults to off because text output gets real messy with it on
-    def __init__(self, model_name, torch_type = torch.float16, display_thoughts = False):
+    def __init__(self, model_name, agent_name = "Ada", torch_type = torch.float16, display_thoughts = False):
+        if not agent_name.isalpha():
+            raise Exception("agent_name must be alphabetic")
+        self.agent_name = agent_name.capitalize()
         self.model_name = model_name
         self.model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch_type)
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
